ac9922af44027cd086204106165f6cd5d726aea7
GKephart <gkephart@cnm.edu>
2018-08-29 11:10:00 -0600

    created the posts route and checked to see if the posts route works

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,12 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
 import {PostService} from "../shared/services/post.service";
+import {Post} from "../shared/interfaces/post";
 
 
 
 @Component({
 	template: require("./posts.template.html")
 })
-export class PostsComponent{
+
+
+export class PostsComponent implements  OnInit{
+	posts: Post[] = [];
+
 	constructor( protected postService : PostService) {
+	}
+
+	ngOnInit(){
+		this.loadPosts()
+	}
+
+	loadPosts() {
+		this.postService.getAllPosts().subscribe(posts => this.posts = posts )
 	}
 }
