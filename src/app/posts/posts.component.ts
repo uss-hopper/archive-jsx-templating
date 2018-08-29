a65a8e0d63d5b402b8d76cae1c199bc9b9622967
GKephart <gkephart@cnm.edu>
2018-08-29 11:59:00 -0600

    wired the post form and tested it

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from "@angular/core";
 
 import {PostService} from "../shared/services/post.service";
 import {Post} from "../shared/interfaces/post";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Status} from "../shared/interfaces/status";
 
 
 
@@ -12,15 +14,34 @@ import {Post} from "../shared/interfaces/post";
 
 export class PostsComponent implements  OnInit{
 	posts: Post[] = [];
+	postForm : FormGroup;
+	status: Status = {status: null, message: null, type: null };
 
-	constructor( protected postService : PostService) {
+	constructor( protected postService : PostService, protected formBuilder: FormBuilder) {
 	}
 
 	ngOnInit(){
-		this.loadPosts()
+		this.loadPosts();
+		this.postForm = this.formBuilder.group({
+			postContent: ["", [Validators.maxLength(1024), Validators.required]],
+			postTitle : ["", [Validators.maxLength(64) , Validators.required]]
+		})
 	}
 
 	loadPosts() {
 		this.postService.getAllPosts().subscribe(posts => this.posts = posts )
 	}
+
+	createPost() {
+		let post: Post = {postId: null, postContent: this.postForm.value.postContent, postDate: null, postTitle: this.postForm.value.postTitle};
+
+		this.postService.createPost(post).subscribe(status =>{
+			this.status = status;
+			if(status.status === 200) {
+				this.loadPosts()
+			}
+		});
+	}
+
+
 }
