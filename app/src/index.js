f1d19a2fcb2f6a5b0e60d5d68c6b6deb5fe885f0
george <gkephart@cnm.edu>
2019-06-06 09:51:41 -0600

    added needed files for routing

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {Home} from "./pages/Home";
+import {FourOhFour} from "./pages/FourOhFour";
+const Routing = () => (
+	<>
+
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Home}/>
+					<Route component={FourOhFour}/>
+				</Switch>
+			</BrowserRouter>
+
+	</>
+);
 
 
-const App = () => ( <h1 className="text-info">hello world</h1> );
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<Routing/>, document.querySelector('#root'));
---
+++ app/src/pages/FourOhFour.js
@@ -0,0 +1,2 @@
+import React from "react"
+export const FourOhFour = () => (<h1>Y U NO FIND</h1>);
