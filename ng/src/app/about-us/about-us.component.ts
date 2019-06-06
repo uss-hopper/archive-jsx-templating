5358144200e790ba91e4f4b5a33abc5ac8ea42aa
george <gkephart@cnm.edu>
2019-06-06 09:25:21 -0600

    innitial commit'

--- ng/src/app/about-us/about-us.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-	templateUrl: "about-us.component.html"
-
-})
-
-export class AboutUsComponent {}
--- ng/src/app/shared/classes/post.ts
+++
@@ -1,6 +0,0 @@
-export interface Post{
-	postId: number,
-	postContent: string,
-	postDate: Date,
-	postTitle: string
-}
