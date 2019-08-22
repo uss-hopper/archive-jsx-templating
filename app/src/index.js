3df26a948b9a614828a94f3332ee4d30189b036a
george <gkephart@cnm.edu>
2019-08-22 14:27:39 -0600

    react is up and running

---
+++ app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {FourOhFour} from "./pages/FourOhFour";
+import {Home} from "./pages/Home";
+
+const Routing = () => (
+	<>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route component={FourOhFour}/>
+			</Switch>
+		</BrowserRouter>
+	</>
+);
+ReactDOM.render(<Routing/>, document.querySelector('#root'));
