6686e0794380872869d45840f81bbadbb4b1abbd
GKephart <gkephart@cnm.edu>
2019-08-22 21:41:16 -0600

    implemented a react bootstarp navbar and added a few missing files I forgot to git add

--- app/src/index.js
+++ app/src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {AboutUs} from "./pages/AboutUs";
 import "./index.css";
+import {faHome, faPencilAlt, faSortDown, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {NavBar} from "./shared/components/NavBar";
+
+library.add(faPencilAlt, faUserCircle, faSortDown);
 
 const Routing = () => (
 	<>
+
 		<BrowserRouter>
+			<NavBar/>
 			<Switch>
 				<Route exact path="/about-us" component={AboutUs} />
 				<Route exact path="/" component={Home} />
--- app/src/shared/components/NavBar.js
+++ app/src/shared/components/NavBar.js
@@ -1,38 +1,37 @@
 import React from "react"
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import NavLink from "react-bootstrap/NavLink";
+
+import {Link} from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
 export const NavBar = () => (
 	<>
 		<header>
-			<Navbar bg="dark"  expand="lg">
-				<NavBar.Brand
-					href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app-5/static-ui/home-view.php">=^ Meow App 5
-					^=</NavBar.Brand><small className="d-none d-md-inline-block text-muted mr-auto"><em>An Angularific
-				Demo.</em></small>
-				<Button className="navbar-toggler" type="button" data-toggle="collapse"
-						  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-						  aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</Button>
-
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav ml-auto">
-						<li className="nav-item dropdown">
-							<a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
-								data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Hello, {{username}}
-							</a>
-							<div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-								<a className="dropdown-item" href="#"><i className="fa fa-user"></i>&nbsp;&nbsp;My Profile</a>
-								<a className="dropdown-item" href="posts-view.php"><i
-									className="fa fa-pencil" />&nbsp;&nbsp;New Post</a>
-								<div className="dropdown-divider" />
-								<a className="dropdown-item" href="#"><i className="fa fa-sign-out"></i>&nbsp;&nbsp;Sign Out</a>
-							</div>
-						</li>
-					</ul>
-				</div>
+			<Navbar bg="dark" expand="sm" variant="dark">
+				<Link exact to="/"><Navbar.Brand>ReactInfinity ÷ -0</Navbar.Brand> </Link>
+				<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+				<Navbar.Collapse>
+					<Navbar.Text>
+						Signed in as: <a href="*">Foo Bar Baz</a>
+					</Navbar.Text>
+					<NavDropdown title="CLICK ME" bg="dark" varient="dark" id="basic-nav-dropdown">
+						<NavLink>
+							<Link exact to="/about-us">
+								About Us <FontAwesomeIcon icon="user-circle"/>
+							</Link>
+						</NavLink>
+						<NavLink>
+							<Link exact to="/posts">
+								Posts <FontAwesomeIcon icon="pencil-alt"/>
+							</Link>
+						</NavLink>
+					</NavDropdown>
+				</Navbar.Collapse>
 			</Navbar>
 		</header>
 	</>
-)
+);
