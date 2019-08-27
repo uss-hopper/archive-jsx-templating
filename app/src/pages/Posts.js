c316a550c45ba4621f20dc48e639e239663d8a4a
george <gkephart@cnm.edu>
2019-08-27 11:40:09 -0600

    quick update to the post form.

--- app/src/pages/Posts.js
+++ app/src/pages/Posts.js
@@ -24,38 +24,46 @@ export const Posts = () => {
 
 					<Row>
 
-							<Card bg="shadow-light" className="border-0 rounded-6 col" >
-								<Card.Body>
-									<Card.Text>
-										<Form>
-											<Form.Group>
-												<InputGroup>
-													<InputGroup.Prepend>
-														<InputGroup.Text>
-															<FontAwesomeIcon icon="dog"/>
-														</InputGroup.Text>
-													</InputGroup.Prepend>
-													<FormControl as="textarea" placeholder="Meow Meow Goes Here"/>
-													<InputGroup.Append> <Button variant="primary" type="submit"> Submit <FontAwesomeIcon icon="envelope"/>
-													</Button></InputGroup.Append>
-												</InputGroup>
-											</Form.Group>
+						<Card bg="shadow-light" className="border-0 rounded-6 col">
+							<Card.Body>
+								<Card.Text>
+									<Form>
+										<Form.Group>
+											<InputGroup>
+												<FormControl placeholder="Title Goes Here"/>
+											</InputGroup>
+										</Form.Group>
 
-										</Form>
-									</Card.Text>
-								</Card.Body>
-							</Card>
-						<CardColumns className="p-4">
-						<Card className="card text-center">
-							<div className="card-body">
-								<Card.Title>Card title</Card.Title>
-								<Card.Text >
-									<p>This card has supporting text below as a natural lead-in to additional content</p>
-									<p><small className="text-muted">Last updated 3 mins ago</small></p>
+										<Form.Group>
+											<InputGroup>
+												<InputGroup.Prepend>
+													<InputGroup.Text>
+														<FontAwesomeIcon icon="dog"/>
+													</InputGroup.Text>
+												</InputGroup.Prepend>
+												<FormControl as="textarea" placeholder="Meow Meow Goes Here"/>
+												<InputGroup.Append>
+													<Button variant="primary" type="submit"> Submit <FontAwesomeIcon
+														icon="envelope"/></Button>
+												</InputGroup.Append>
+											</InputGroup>
+										</Form.Group>
+
+									</Form>
 								</Card.Text>
-							</div>
+							</Card.Body>
 						</Card>
-					</CardColumns>
+						<CardColumns className="p-4">
+							<Card className="card text-center">
+								<div className="card-body">
+									<Card.Title>Card title</Card.Title>
+									<Card.Text>
+										<p>This card has supporting text below as a natural lead-in to additional content</p>
+										<p><small className="text-muted">Last updated 3 mins ago</small></p>
+									</Card.Text>
+								</div>
+							</Card>
+						</CardColumns>
 					</Row>
 
 				</Container>
